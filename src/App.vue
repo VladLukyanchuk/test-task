83b1fba8cca3b64e6164de25f93bf4cddf492281
Vlad_Lukyanchuk <vladyslav.lukyanchuk@gmail.com>
2023-04-18 19:03:47 +0300

    Fist settings, reset and main style added. Header created

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,24 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <Header @open-nav="openNavigation"></Header>
+  <navigation v-if="navigation"/>
+  <div>app</div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Header from "@/components/HeaderComponent.vue";
+import Navigation from "@/components/NavigationComponent.vue";
 
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  data() {
+    return {
+      navigation: true,
+    }
+  },
+  components: { Header, Navigation },
+  methods: {
+    openNavigation() {
+      console.log("openNavigation");
     }
   }
-}
-</style>
+};
+</script>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,56 @@
+<template>
+  <header class="header">
+    <div class="header__container">
+      <button
+        @click="this.$emit('open-nav')"
+        class="burger-button"
+        type="button"
+        role="button"
+        aria-label="open/close navigation"
+      >
+        <span></span><span></span><span></span>
+      </button>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  width: 100%;
+  height: 46px;
+}
+.header__container {
+  display: flex;
+  align-items: center;
+  padding: 15px;
+  height: 100%;
+}
+
+.burger-button {
+  all: unset;
+  transition: all 200ms ease-in-out;
+  cursor: pointer;
+  display: block;
+  outline: none;
+  width: 24px;
+  height: 16px;
+
+  span {
+    background-color: white;
+    display: block;
+    width: 100%;
+    height: 2px;
+  }
+  span:nth-child(2) {
+    margin: 4px 0;
+  }
+}
+</style>
